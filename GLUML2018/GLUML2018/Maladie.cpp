dd3545fef4295ffe3cbbdac8ebb8b3cdef19fede
warsai83 <william.occelli@hotmail.fr>
2018-05-02 17:14:12 +0200

    Maladie

--- GLUML2018/GLUML2018/Maladie.cpp
+++ GLUML2018/GLUML2018/Maladie.cpp
@@ -10,3 +10,20 @@ Maladie::Maladie()
 Maladie::~Maladie()
 {
 }
+
+void Maladie::AjouterEmpreinte(Empreinte emp)
+{
+	list<Empreinte>::iterator it = ListeEmpreintes.begin();
+	ListeEmpreintes.insert(it,emp);
+}
+
+string Maladie::toString()
+{
+	string s = Name + " :\r\n";
+	list<Empreinte>::iterator it;
+	for (it = ListeEmpreintes.begin(); it != ListeEmpreintes.end(); ++it)
+	{
+		s += (*it).toString() + "\r\n";
+	}
+	return s;
+}
--- GLUML2018/GLUML2018/Maladie.h
+++ GLUML2018/GLUML2018/Maladie.h
@@ -1,10 +1,16 @@
 #pragma once
+#include <list>
+#include "Empreinte.h"
+
 class Maladie
 {
 public:
 	Maladie();
 	~Maladie();
+	void AjouterEmpreinte(Empreinte emp);
+	string toString();
 private: 
-
+	string Name;
+	list <Empreinte> ListeEmpreintes; 
 };
 
