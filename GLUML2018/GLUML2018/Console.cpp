a4b59b9a08a650138682a9ac81a813f6b2cf07f8
halunka <mas.mat@hotmail.fr>
2018-05-02 17:53:44 +0200

    Console class

---
+++ GLUML2018/GLUML2018/Console.cpp
@@ -0,0 +1,112 @@
+/*************************************************************************
+                           Console  -  description
+    Classe permettant la'affichage sur la console de visual studio
+                             -------------------
+    début                : 02-05-2018
+    copyright            : (C) 2018 par Halunka Matthieu, Mathilde Moureau
+										Stanley Cheah, William Occelli
+    e-mail               : matthieu.halunka@insa-lyon.fr
+                           mathilde.moureau@insa-lyon.fr
+						   stanley.cheah@insa-lyon.fr
+						   william.occelli@insa-lyon.fr
+
+*************************************************************************/
+
+//---------- Réalisation de la classe <Analog> (fichier Console.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+
+using namespace std;
+#include <iostream>
+#include <cstring>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <deque>
+#include <map>
+#include <algorithm>
+
+//------------------------------------------------------ Include personnel
+#include "Console.h"
+
+//------------------------------------------------------------- Constantes
+const char BLEU [] = {033,'[','3','4','m'};
+const char ROUGE [] = {033,'[','3','1','m'};
+const char JAUNE [] = {033,'[','3','3','m'};
+const char RESET [] = {033,'[','m',017};
+//----------------------------------------------------------------- PUBLIC
+//----------------------------------------------------- Méthodes publiques
+
+#ifdef DEBUG
+// ostream & debug ( ostream & os )
+// Algorithme :
+//      Change les paramètres du ostream pour qu'il écrive en bleu.
+ostream & debug (ostream & os){
+    os.write(BLEU, sizeof(BLEU));
+    return os;
+}//----- Fin de debug
+#endif
+
+// ostream & erreur ( ostream & os )
+// Algorithme :
+//      Change les paramètres du ostream pour qu'il écrive en rouge.
+ostream & erreur (ostream & os){
+    os.write(ROUGE, sizeof(ROUGE));
+    return os;
+}//----- Fin de erreur
+
+// ostream & attention( ostream & os )
+// Algorithme :
+//      Change les paramètres du ostream pour qu'il écrive en jaune.
+ostream & attention (ostream & os){
+    os.write(JAUNE, sizeof(JAUNE));
+    return os;
+}//----- Fin de attention
+
+// ostream & debug ( ostream & os )
+// Algorithme :
+//      Change les paramètres du ostream pour qu'il reprenne ses
+//      paramètres initiaux.
+ostream & raz (ostream & os){
+    os.write(RESET, sizeof(RESET));
+    return os;
+}//----- Fin de debug
+
+void afficherErreur(string err){
+	cout << erreur << err << raz << endl;
+}
+
+void afficherDanger(string danger) {
+	cout << attention << danger << raz << endl;
+}
+
+void afficherMessage(string msg) {
+
+}
+
+int lireEntier() {
+}
+
+string lireChaine() {
+	string lecture;
+	lecture << cin;
+	return lecture;
+}
+
+
+
+//-------------------------------------------- Constructeurs - destructeur
+
+Console::Console(const Console & uneConsole) {
+	//INTERDIT
+}
+
+Console::Console(){
+
+}
+
+Console::~Console() {
+
+}
---
+++ GLUML2018/GLUML2018/Console.h
@@ -0,0 +1,99 @@
+/*************************************************************************
+					Console  -  description
+		Classe permettant la'affichage sur la console de visual studio
+							-------------------
+	début                : 02-05-2018
+	copyright            : (C) 2018 par Halunka Matthieu, Mathilde Moureau
+										Stanley Cheah, William Occelli
+    e-mail               : matthieu.halunka@insa-lyon.fr
+                           mathilde.moureau@insa-lyon.fr
+						   stanley.cheah@insa-lyon.fr
+						   william.occelli@insa-lyon.fr
+*************************************************************************/
+
+//---------- Interface du module <Console> (fichier Console.h) -------------------
+#if ! defined ( Console_H )
+#define Console_H
+
+//------------------------------------------------------------------------
+// Rôle du module <Console>
+//
+//
+//------------------------------------------------------------------------
+
+/////////////////////////////////////////////////////////////////  INCLUDE
+//--------------------------------------------------- Interfaces utilisées
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//////////////////////////////////////////////////////////////////  PUBLIC
+//---------------------------------------------------- Fonctions publiques
+// type Nom ( liste de paramètres );
+// Mode d'emploi :
+//
+// Contrat :
+//
+class Console {
+
+#ifdef DEBUG
+	// ostream & debug ( ostream & os )
+	ostream & debug(ostream & os);
+#endif
+
+	 // ostream & erreur ( ostream & os )
+	ostream & erreur(ostream & os);
+
+	 // ostream & attention( ostream & os )
+	ostream & attention(ostream & os);
+
+	 // ostream & debug ( ostream & os )
+	ostream & raz(ostream & os);
+
+	//----------------------------------------------------------------- PUBLIC
+public:
+	//----------------------------------------------------- Méthodes publiques
+	void afficherErreur(string err);
+
+	void afficherDanger(string danger);
+
+	void afficherMessage(string msg);
+
+	int lireEntier();
+
+	string lireChaine();
+	//-------------------------------------------- Constructeurs - destructeur
+
+	Console(const Console & uneConsole);
+	// Mode d'emploi (constructeur de copie) :
+	//  Usage interdit !
+	// Contrat :
+	//  Aucun
+
+	Console();
+	// Mode d'emploi :
+	//  Permet de créer l'objet et d'initialiser ses attributs.
+	// Contrat :
+	//		Aucun
+
+	virtual ~Console();
+	// Mode d'emploi :
+	//  
+	// Contrat :
+	//  Aucun
+
+	//----------------------------------------------------- Attributs publiques
+
+	//----------------------------------------------------- Attributs protégés
+protected:
+	/*ostream sortieConsole;
+	ostream erreurConsole;
+	istream entreeConsole;*/
+};
+
+
+#endif // Console_H
+
+
+
