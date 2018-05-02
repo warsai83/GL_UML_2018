b60d9fcc10024345560e0dd78e3030f8c837743a
warsai83 <william.occelli@hotmail.fr>
2018-05-02 16:11:39 +0200

    Empreinte

--- GLUML2018/GLUML2018/Empreinte.cpp
+++ GLUML2018/GLUML2018/Empreinte.cpp
@@ -10,3 +10,9 @@ Empreinte::Empreinte()
 Empreinte::~Empreinte()
 {
 }
+
+string Empreinte::toString()
+{
+	string s = to_string(NoID) + ";" + to_string(A1) +";"+ to_string(A2) +";"+ to_string(A3) +";"+ to_string(A4) +";"+ to_string(AZ51) +";"+ Disease;
+	return s;
+}
--- GLUML2018/GLUML2018/Empreinte.h
+++ GLUML2018/GLUML2018/Empreinte.h
@@ -1,8 +1,19 @@
 #pragma once
+#include <string>
+
+using namespace std;
+
 class Empreinte
 {
 public:
 	Empreinte();
 	~Empreinte();
+
+	string toString();
+
+private:
+	int NoID;
+	double A1, A2, A3, A4, AZ51;
+	string Disease; 
 };
 
