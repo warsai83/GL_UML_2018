b26d3043802d9edd75161ead24051d36fe59de6a
MrMathilde <38728863+MrMathilde@users.noreply.github.com>
2018-05-23 19:47:29 +0200

    Update Formateur.cpp

--- GLUML2018/GLUML2018/Formateur.cpp
+++ GLUML2018/GLUML2018/Formateur.cpp
@@ -33,71 +33,41 @@ using namespace std;
 //      Décompose la trame obtenue en éléments simples
 //      puis formate certains éléments pour les rendre exploitables
 //      par la classe Analog.
-void Formateur::Decouper(string ligneLue="") {
-	cout <<"decoupeur de form"<< endl;
+vector<vector<string>> Formateur::Decouper(string ligneLue="") {
     if (ligneLue=="")
     {
-	cout <<"if decoup vide"<< endl;
-        return;
+        return tabEmp;
     }
-	cout <<"sep"<< endl;
-    istringstream separateur(ligneLue);
+	string temp;
+	stringstream ss(ligneLue);
+	vector<string>* v = new vector<string>();
+	getline(ss,NoID,';');
+	getline(ss,A1,';');
+	getline(ss,A2,';');
+	getline(ss,A3,';');
+	getline(ss,A4,';');
+	getline(ss,AZ51,';');
+	
+	v->push_back(NoID);
+	v->push_back(A1);
+	v->push_back(A2);
+	v->push_back(A3);
+	v->push_back(A4);
+	v->push_back(AZ51);
+	tabEmp.push_back(*v);
 
-    string time1;
-    string time2;
-    string time3;
-    string request1;
-    string request2;
-    string request3;
-    string idNav1;
+	delete(v);
+	cout << "empreinte brute --------------------------------" << endl;
+    	cout << NoID << endl;
+	cout << A1 << endl;
+	cout << A2 << endl;
+	cout << A3 << endl;
+	cout << A4 << endl;
+	cout << AZ51<< endl;
+	cout << "vecteurGen----------------------------------------------" << endl;
+	//delete(v); A FAIRE PLUS TARD
+    return tabEmp;
 
-    separateur >> IP >> userLog >> authUser
-               >> time1 >> time2  >> request1 >> request2 >> request3
-               >> code >> length >> referer
-               >> idNav1;
-
-    idNav1.erase(0, 1);
-    idNav = idNav1;
-    // on gère le cas ou l'idNav est de la forme "idNav" et non "idNav blabla"
-    if (idNav1[idNav1.size()-1]=='"') {
-	cout <<"if decoup"<< endl;
-        idNav.erase(idNav1.size()-1, 1);
-    } else {
-	cout <<"else decoup"<< endl;
-        separateur >> idNav1;
-        while (idNav1[idNav1.size()-1]!='"'){
-		cout <<"while"<< endl;
-            idNav = idNav + ' ' + idNav1;
-            separateur >> idNav1;
-        }
-cout <<"fin while"<< endl;
-        idNav1.erase(idNav1.size()-1, 1);
-        idNav = idNav + ' ' + idNav1;
-    }
-cout <<"fin meth"<< endl;
-    time = time1 + ' ' + time2; // on enleve les caractères '[' et ']'
-    time.erase(0, 1);
-    time.erase(time.size()-1, 1);
-
-    // on efface les caractères '"'
-    referer.erase(0, 1);
-    referer.erase(referer.size()-1, 1);
-
-    request1.erase(0, 1); // efface "
-    request3.erase(request3.size()-1, 1);
-
-    typeRequest = request1;
-    target = request2;
-    httpEnd = request3;
-
-#ifdef DEBUG_FORMATEUR
-    cout << IP << endl << userLog <<endl<< authUser << endl
-         << time << endl << typeRequest << endl << target << endl
-         << httpEnd << endl << code << endl << length << endl << referer << endl
-         << idNav << endl;
-#endif
-
-    return;
 }//Fin de Decouper
 
 //-------------------------------------------- Constructeurs - destructeur
@@ -108,17 +78,12 @@ Formateur::Formateur ( const Formateur & unFormateur )
 #ifdef MAP
     cout << "Appel au constructeur de copie de <Formateur>" << endl;
 #endif
-    IP=unFormateur.IP;
-    userLog=unFormateur.userLog;
-    authUser=unFormateur.authUser;
-    time=unFormateur.time;
-    typeRequest=unFormateur.typeRequest;
-    target=unFormateur.target;
-    httpEnd=unFormateur.httpEnd;
-    code=unFormateur.code;
-    length=unFormateur.length;
-    referer=unFormateur.referer;
-    idNav=unFormateur.idNav;
+    	NoID=unFormateur.NoID;
+   	A1=unFormateur.A1;
+    	A2=unFormateur.A2;
+    	A3=unFormateur.A3;
+    	A4=unFormateur.A4;
+	AZ51=unFormateur.AZ51;
 } //----- Fin de Formateur (constructeur de copie)
 
 
@@ -129,17 +94,13 @@ Formateur::Formateur ()
 #ifdef MAP
     cout << "Appel au constructeur de <Formateur>" << endl;
 #endif
-    IP="";
-    userLog="";
-    authUser="";
-    time="";
-    typeRequest="";
-    target="";
-    httpEnd="";
-    code="";
-    length="";
-    referer="";
-    idNav="";
+	
+	NoID="";
+	A1="";
+	A2="";
+	A3="";
+	A4="";
+	AZ51="";
 } //----- Fin de Formateur
 
 
