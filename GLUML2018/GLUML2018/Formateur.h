ba16613d72cd2e036726a8c170959fc217eeb624
MrMathilde <38728863+MrMathilde@users.noreply.github.com>
2018-05-23 16:11:40 +0200

    Update Formateur.h

--- GLUML2018/GLUML2018/Formateur.h
+++ GLUML2018/GLUML2018/Formateur.h
@@ -12,142 +12,69 @@
 						   william.occelli@insa-lyon.fr
 *************************************************************************/
 
-//---------- Réalisation de la classe <Formateur> (fichier Formateur.cpp) ------------
+//---------- Interface de la classe <Formateur> (fichier Formateur.h) ----------------
+#if ! defined ( FORMATEUR_H )
+#define FORMATEUR_H
 
-//---------------------------------------------------------------- INCLUDE
+//--------------------------------------------------- Interfaces utilisées
+#include <string>
+//------------------------------------------------------------------------
+// Rôle de la classe <Formateur>
+//      Permet de découper les requêtes provenant du log du serveur apache
+//      afin de les stocker dans un ensemble d'attributs connus
+//      et de les rendres ainsi lisibles pour la classe Analog.
+//------------------------------------------------------------------------
 
-//-------------------------------------------------------- Include système
-using namespace std;
-#include <iostream>
-#include <sstream>
-
-//------------------------------------------------------ Include personnel
-#include "Formateur.h"
-
-//------------------------------------------------------------- Constantes
-
+class Formateur
+{
 //----------------------------------------------------------------- PUBLIC
+
+public:
 //----------------------------------------------------- Méthodes publiques
-// void Formateur::Decouper(string ligneLue="")
-// Algorithme :
-//      Décompose la trame obtenue en éléments simples
-//      puis formate certains éléments pour les rendre exploitables
-//      par la classe Analog.
-void Formateur::Decouper(string ligneLue="") {
-	cout <<"decoupeur de form"<< endl;
-    if (ligneLue=="")
-    {
-	cout <<"if decoup vide"<< endl;
-        return;
-    }
-	cout <<"sep"<< endl;
-    istringstream separateur(ligneLue);
-
-    string time1;
-    string time2;
-    string time3;
-    string request1;
-    string request2;
-    string request3;
-    string idNav1;
-
-    separateur >> IP >> userLog >> authUser
-               >> time1 >> time2  >> request1 >> request2 >> request3
-               >> code >> length >> referer
-               >> idNav1;
-
-    idNav1.erase(0, 1);
-    idNav = idNav1;
-    // on gère le cas ou l'idNav est de la forme "idNav" et non "idNav blabla"
-    if (idNav1[idNav1.size()-1]=='"') {
-	cout <<"if decoup"<< endl;
-        idNav.erase(idNav1.size()-1, 1);
-    } else {
-	cout <<"else decoup"<< endl;
-        separateur >> idNav1;
-        while (idNav1[idNav1.size()-1]!='"'){
-		cout <<"while"<< endl;
-            idNav = idNav + ' ' + idNav1;
-            separateur >> idNav1;
-        }
-cout <<"fin while"<< endl;
-        idNav1.erase(idNav1.size()-1, 1);
-        idNav = idNav + ' ' + idNav1;
-    }
-cout <<"fin meth"<< endl;
-    time = time1 + ' ' + time2; // on enleve les caractères '[' et ']'
-    time.erase(0, 1);
-    time.erase(time.size()-1, 1);
-
-    // on efface les caractères '"'
-    referer.erase(0, 1);
-    referer.erase(referer.size()-1, 1);
-
-    request1.erase(0, 1); // efface "
-    request3.erase(request3.size()-1, 1);
-
-    typeRequest = request1;
-    target = request2;
-    httpEnd = request3;
-
-#ifdef DEBUG_FORMATEUR
-    cout << IP << endl << userLog <<endl<< authUser << endl
-         << time << endl << typeRequest << endl << target << endl
-         << httpEnd << endl << code << endl << length << endl << referer << endl
-         << idNav << endl;
-#endif
-
-    return;
-}//Fin de Decouper
+    // void Decouper(string ligneLue);
+    // Mode d'emploi :
+    //     Cette méthode découpe une requête/ligne de fichier de log
+    //     de serveur Apache pour ensuite stocker les informations
+    //     obtenues dans les attributs de notre objet.
+    // Contrat :
+    //     La requête proposée en entrée sera supposé comme étant
+    //     correctement formée et entière.
+    void Decouper(string ligneLue);
 
 //-------------------------------------------- Constructeurs - destructeur
-Formateur::Formateur ( const Formateur & unFormateur )
-// Algorithme :
-//      Copie la valeur de chaque attributs dans le nouvel objet
-{
-#ifdef MAP
-    cout << "Appel au constructeur de copie de <Formateur>" << endl;
-#endif
-    IP=unFormateur.IP;
-    userLog=unFormateur.userLog;
-    authUser=unFormateur.authUser;
-    time=unFormateur.time;
-    typeRequest=unFormateur.typeRequest;
-    target=unFormateur.target;
-    httpEnd=unFormateur.httpEnd;
-    code=unFormateur.code;
-    length=unFormateur.length;
-    referer=unFormateur.referer;
-    idNav=unFormateur.idNav;
-} //----- Fin de Formateur (constructeur de copie)
+    Formateur ( const Formateur & unFormateur );
+    // Mode d'emploi (constructeur de copie) :
+    //      Copie l'ensemble des attributs de l'objet
+    //      passé par référence.
+    // Contrat :
+    //      Aucun
 
+    Formateur ();
+    // Mode d'emploi :
+    //      Crée et d'initialise notre objet.
+    // Contrat :
+    //      Aucun
 
-Formateur::Formateur ()
-// Algorithme :
-//      Initialise les attributs de notre objet
-{
-#ifdef MAP
-    cout << "Appel au constructeur de <Formateur>" << endl;
-#endif
-    IP="";
-    userLog="";
-    authUser="";
-    time="";
-    typeRequest="";
-    target="";
-    httpEnd="";
-    code="";
-    length="";
-    referer="";
-    idNav="";
-} //----- Fin de Formateur
+    virtual ~Formateur ( );
+    // Mode d'emploi :
+    //      Détruit l'objet.
+    // Contrat :
+    //      Aucun
 
+    string IP;
+    string userLog;
+    string authUser;
+    string time;
+    string typeRequest;
+    string target;
+    string httpEnd;
+    string code;
+    string length;
+    string referer;
+    string idNav;
 
-Formateur::~Formateur ( )
-// Algorithme :
-//      Aucun
-{
-#ifdef MAP
-    cout << "Appel au destructeur de <Formateur>" << endl;
-#endif
-} //----- Fin de ~Formateur
+};
+
+//-------------------------------- Autres définitions dépendantes de <Formateur>
+
+#endif // FORMATEUR_H
