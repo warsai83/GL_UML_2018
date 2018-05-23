5d4bcec7d21136cd5a6d6e8d65356c607ba31f8e
warsai83 <william.occelli@hotmail.fr>
2018-05-23 17:17:44 +0200

    attribut maladie d'empreinte

--- GLUML2018/GLUML2018/Empreinte.cpp
+++ GLUML2018/GLUML2018/Empreinte.cpp
@@ -21,7 +21,7 @@ william.occelli@insa-lyon.fr
 
 string Empreinte::toString()
 {
-	string s = to_string(NoID) + ";" + to_string(A1) + ";" + to_string(A2) + ";" + to_string(A3) + ";" + to_string(A4) + ";" + to_string(AZ51) + ";" + Disease.toString();
+	string s = to_string(NoID) + ";" + to_string(A1) + ";" + to_string(A2) + ";" + to_string(A3) + ";" + to_string(A4) + ";" + to_string(AZ51) + ";" + Disease;
 	return s;
 }
 
@@ -30,14 +30,14 @@ Maladie Empreinte::getDisease() const {
 }
 
 //-------------------------------------------- Constructeurs - destructeur
-Empreinte::Empreinte(int unNoID, double unA1, double unA2, double unA3, double unA4, double unAZ51, Maladie uneDisease) : NoID(unNoID), A1(unA1), A2(unA2), A3(unA3), A4(unA4), AZ51(unAZ51), Disease(uneDisease)
+Empreinte::Empreinte(int unNoID, double unA1, double unA2, double unA3, double unA4, double unAZ51, string uneDisease) : NoID(unNoID), A1(unA1), A2(unA2), A3(unA3), A4(unA4), AZ51(unAZ51), Disease(uneDisease)
 {
 
 }
 
 Empreinte::Empreinte()
 {
-	Disease = Maladie();
+	Disease = "";
 }
 
 Empreinte::~Empreinte()
