eb41968cc7279e3b947c68fa199a495469df5471
firemat <mas.mat@hotmail.fr>
2018-06-09 01:07:26 +0200

    Structuration finale

---
+++ GLUML2018/Maladif/Vue/Affichage.cpp
@@ -0,0 +1,167 @@
+/*************************************************************************
+                            Affichage  -  description
+            Classe gérant l'interaction avec l'utilisateur
+                            -------------------
+début                : 02-05-2018
+copyright            : (C) 2018 par Halunka Matthieu, Mathilde Moureau
+                                    Stanley Cheah, William Occelli
+e-mail               :  matthieu.halunka@insa-lyon.fr
+                        mathilde.moureau@insa-lyon.fr
+                        stanley.cheah@insa-lyon.fr
+                        william.occelli@insa-lyon.fr
+
+*************************************************************************/
+
+//--- Réalisation de la classe <Affichage> (fichier Affichage.cpp) -------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+
+//------------------------------------------------------ Include personnel
+#include "Affichage.h"
+
+//------------------------------------------------------------- Constantes
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+void SeparerString(string* result, string* s, const char separateur=' ')
+{
+	size_t pos1 = s->find(separateur);
+	result[0] = s->substr(0, pos1);
+    result[1] = s->substr(pos1 + 1, s->length());
+}
+
+
+int main()
+{
+	//const string CHEMIN_RACINE="../Maladif/Maladif/";
+	//const string CHEMIN_RACINE ="C:\\Users\\William\\Desktop\\Jeux (2)\\GitHub\\GL_UML_2018\\Maladif\\Maladif\\" ;
+    const string CHEMIN_RACINE="./";
+    //const string CHEMIN_RACINE="../../Maladif/Maladif/";
+	//initialiserEmpreintes();
+	c->afficherMessage("Bienvenue sur le service Malad'If ! \r\n");
+	bool continuer=true;
+	while (continuer)
+	{
+
+		c->afficherMessage(" Saisir une action a effectuer : \r\n");
+		string commandeUtilisateur = c->lireChaine();
+		string commande[2] ;
+		SeparerString(commande,&commandeUtilisateur);
+
+#ifdef DEBUG
+		c->afficherDebug("res 0 : "+commande[0]);
+		c->afficherDebug("res 1  : "+commande[1]);
+#endif
+		//-----------
+		if (commande[0]=="ANALYSE")
+		{
+
+			string nomFichier = CHEMIN_RACINE+commande[1];
+			string format;
+            format=commande[1].substr((commande[1].length())-4,commande[1].length());
+            if(format==".txt") {
+                c->afficherMessage("Demande d'analyse ...");
+                vector<Empreinte> res = Gestion::AnalyseEmpreinte(listeEmpreinte, nomFichier);
+                c->afficherMessage(Gestion::AfficherEmpreinte(res));
+            }
+            else
+            {
+                c->afficherErreur("Erreur, le format de l'empreinte n'est pas valide");
+            }
+
+		}
+		else if (commande[0]=="LISTEMALADIES")
+		{
+			c->afficherMessage("Affichage de la liste des maladies ...");
+			set<string> maladiesConnues;
+            if (listeMaladie.begin() == listeMaladie.end())
+            {
+                c->afficherDanger("Warning, la base ne contient pas de maladie référencée");
+            }else{
+                Gestion::GetListeMaladie(listeMaladie, maladiesConnues);
+                for (std::set<string>::iterator it = maladiesConnues.begin(); it != maladiesConnues.end(); it++) {
+                    c->afficherMessage(*it + "\r\n");
+                }
+            }
+		}
+		else if (commande[0]=="DETAILSMALADIE")
+		{
+		    if(listeMaladie.empty()){
+		        c->afficherDanger("Warning, la base de maladie est vide");
+		    }
+		    if(commande[1]==commande[0]){
+		        c->afficherErreur("Erreur, impossible de lire la valeur de la maladie");
+		    }else {
+                c->afficherMessage("Affichage des détails de la maladie ...");
+                string nomMaladie = commande[1];
+                vector<Empreinte> detailsMaladie = Gestion::GetDetail(listeMaladie, nomMaladie);
+                if (!detailsMaladie.empty()) {
+                    string listeEmpreinte = Gestion::AfficherEmpreinte(detailsMaladie);
+                    c->afficherMessage(listeEmpreinte);
+                }else{
+                    c->afficherErreur("Erreur, Maladie introuvable");
+                }
+            }
+		}
+		else if (commande[0] == "LOAD")
+		{
+
+			string format;
+			format=commande[1].substr((commande[1].length())-4,commande[1].length());
+			if(format==".txt")
+			{
+				string nomFichier = CHEMIN_RACINE + commande[1];
+			    listeEmpreinte = Gestion::LectureBase(nomFichier);
+			    if(listeEmpreinte.begin()!=listeEmpreinte.end())
+                {
+                    c->afficherMessage("Succes de l'envoi du fichier d'empreintes");
+                    c->afficherMessage("Chargement de BD...");
+                    //Traitement des listeEmpreinte
+					if(!listeEmpreinte.empty()){
+						Gestion::ChargerListeMaladies(listeEmpreinte, listeMaladie);
+					}
+                    c->afficherMessage("Chargement termine");
+                }
+			}
+
+			else
+			{
+				c->afficherErreur("Erreur, le format de l'empreinte n'est pas valide");
+			}
+		}
+        else if (commande[0] == "LIMIT")
+		{
+			c->afficherMessage("Le nombre d'éléments empreintes maximum stockable est de :");
+			c->afficherTaille(listeEmpreinte.max_size());
+            c->afficherMessage("Le nombre d'éléments maladies maximum stockable est de :");
+            c->afficherTaille(listeMaladie.max_size());
+        }
+		else if (commande[0] == "QUITTER")
+		{
+			continuer = false;
+		}
+		else
+		{
+			c->afficherDanger("Commande inconnue");
+		}
+		//----------
+	}
+	return 0;
+}
+//-------------------------------------------- Constructeurs - destructeur
+Affichage::Affichage()
+{
+#ifdef MAP
+	c->afficherMessage("Appel au constructeur de <Affichage>");
+#endif
+}
+
+
+Affichage::~Affichage()
+{
+#ifdef MAP
+	c->afficherMessage("Appel au destructeur de <Affichage>");
+#endif
+}
---
+++ GLUML2018/Maladif/Vue/Console.cpp
@@ -0,0 +1,201 @@
+/*************************************************************************
+                           Console  -  description
+                Classe permettant l'affichage sur l'IHM texte
+                             -------------------
+    début                : 02-05-2018
+    copyright            : (C) 2018 par Halunka Matthieu, Mathilde Moureau
+										Stanley Cheah, William Occelli
+    e-mail               : matthieu.halunka@insa-lyon.fr
+                           mathilde.moureau@insa-lyon.fr
+						   stanley.cheah@insa-lyon.fr
+						   william.occelli@insa-lyon.fr
+*************************************************************************/
+
+//---------- Réalisation de la classe <Console> (fichier Console.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+//------------------------------------------------------ Include personnel
+#include "Console.h"
+
+using namespace std;
+//------------------------------------------------------------- Constantes
+const char BLEU [] = {033,'[','3','4','m'};
+const char ROUGE [] = {033,'[','3','1','m'};
+const char JAUNE [] = {033,'[','3','3','m'};
+const char RESET [] = {033,'[','m',017};
+//----------------------------------------------------------------- PUBLIC
+//----------------------------------------------------- Méthodes publiques
+
+#ifdef DEBUG
+ // afficherDebug(string info)
+ // Algorithme :
+ //      Ecrit les informations de débugage dans la console
+void Console::afficherDebug(const string &info)
+{
+	cout << debug << info << raz << endl;
+}//----- Fin de afficherDebug
+#endif
+
+
+// afficherErreur(string err)
+// Algorithme :
+//      Affiche le message d'erreur sur la console
+void Console::afficherErreur(const string &err){
+	cerr << err << endl;
+}//----- Fin de afficherErreur
+
+// afficherDanger(string danger)
+// Algorithme :
+//      Affiche le message d'avertissement sur la console
+void Console::afficherDanger(const string &danger) {
+	cout << attention << danger << raz << endl;
+}//----- Fin de afficherDanger
+
+// afficherDanger(string danger)
+// Algorithme :
+//      Affiche le message sur la console
+void Console::afficherMessage(const string &msg) {
+	cout << msg << endl;
+}//----- Fin de afficherMessage
+
+void Console::afficherTaille(const size_t &taille) {
+    cout << taille << endl;
+}//----- Fin de afficherMessage
+
+// lireEntier()
+// Algorithme :
+//	Méthode pour lire un entier valide à partir du terminal
+int Console::lireEntier() {
+	int s;
+	string lecture;
+	istringstream separateur;
+	bool charValide = false;
+	char caracLu;
+	while (lecture.empty() || !charValide)
+	{
+		getline(cin, lecture);
+		if (!lecture.empty())
+		{
+			separateur.str(lecture);
+			for (unsigned int i = 0; i < lecture.length(); ++i)
+			{
+				separateur.get(caracLu);
+				//On regarde si la ligne lue est constituée d'autres caractères
+				// que espace et retour chariot.
+				if (caracLu<'0' || caracLu>'9')
+				{
+					charValide = false;
+					afficherErreur("Saissisez un entier !");
+					break;
+				}
+				else {
+					charValide = true;
+				}
+			}
+			separateur.clear();
+
+			if (lecture.length()>6 && charValide)
+			{
+				lecture = "";
+				afficherErreur("Entier trop grand !");
+			}
+		}
+	}
+	s = stoi(lecture);
+	return s;
+}//----- Fin de lireEntier
+
+// lireChaine()
+// Algorithme :
+//	Permet de lire le terminal et d'éviter la saisie de caractères frauduleux
+string Console::lireChaine() {
+	string s;
+	istringstream separateur;
+	bool charValide = false;
+	char caracLu;
+	while (s.empty() || (!charValide))
+	{
+		getline(cin, s);
+		if (!s.empty())
+		{
+			separateur.str(s);
+			for (unsigned int i = 0; i < s.length(); ++i)
+			{
+				separateur.get(caracLu);
+				//On regarde si la ligne lue est constituée d'autres caractères
+				// que espace et retour chariot.
+				if (caracLu != ' '&&caracLu != '\r')
+				{
+					charValide = true;
+					//Si je trouve un /, la saisie est invalide
+					/*if (caracLu == '/') {
+						afficherErreur("Le / est interdit, recommencez la saisie !");
+						charValide = false;
+						break;
+					}*/
+				}
+			}
+			separateur.clear();
+		}
+	}
+	return s;
+}
+
+//-------------------------------------------- Constructeurs - destructeur
+
+Console::Console(const Console & uneConsole) {
+	//INTERDIT
+}
+
+Console::Console(){
+
+}
+
+Console::~Console() {
+
+}
+
+
+// ostream & erreur ( ostream & os )
+// Algorithme :
+//      Change les paramètres du ostream pour qu'il écrive en rouge.
+ostream & Console::erreur (ostream & os){
+	os.write(ROUGE, sizeof(ROUGE));
+	return os;
+}//----- Fin de erreur
+
+// ostream & attention( ostream & os )
+// Algorithme :
+//      Change les paramètres du ostream pour qu'il écrive en jaune.
+ostream & Console::attention (ostream & os){
+	os.write(JAUNE, sizeof(JAUNE));
+	return os;
+}//----- Fin de attention
+
+// ostream & debug ( ostream & os )
+// Algorithme :
+//      Change les paramètres du ostream pour qu'il reprenne ses
+//      paramètres initiaux.
+ostream & Console::raz (ostream & os){
+	os.write(RESET, sizeof(RESET));
+	return os;
+}//----- Fin de debug
+
+#ifdef DEBUG
+// ostream & debug ( ostream & os )
+// Algorithme :
+//      Change les paramètres du ostream pour qu'il écrive en bleu.
+ostream & Console::debug (ostream & os){
+    os.write(BLEU, sizeof(BLEU));
+    return os;
+}//----- Fin de debug
+#endif
